15d3e71e561232e7fabe488f3a6e5dda555598b8
Sudarshan Shetty <sudarshan.gajanan@iwavesystems.com>
2021-01-22 14:01:36 +0530

    Updated folders

---
+++ include/configs/imx8mn_iwg37m_uuu.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2020 iWave System Technologies Pvt Ltd.
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef __IMX8MN_IWG37M_UUU_H
+#define __IMX8MN_IWG37M_UUU_H
+
+#include "imx8mn_iwg37m.h"
+
+#ifdef BSP_VERSION
+#undef BSP_VERSION
+#define BSP_VERSION     "iW-PRGJZ-SC-01-R1.0-REL0.1-Linux5.4.24_UUU"
+#endif
+
+#endif
+
+
